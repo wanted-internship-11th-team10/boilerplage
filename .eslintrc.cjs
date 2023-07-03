3b24dd009bec96b436f08e4874dace15c96b2e4f
kimseunggyu <dev.seunggyu@gmail.com>
2023-07-04 01:43:53 +0900

    chore : unused-imports lint 추가 - 사용하지 않는 import 구문 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh', 'simple-import-sort'],
+  plugins: ['react-refresh', 'simple-import-sort', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     'simple-import-sort/imports': [
@@ -20,5 +20,6 @@ module.exports = {
         ],
       },
     ],
+    'unused-imports/no-unused-imports': 'error',
   },
 };
